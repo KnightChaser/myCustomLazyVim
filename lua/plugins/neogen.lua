b1887a3010ce0e26707470ccfae8d80272f5bfd2
knightchaser <agerio100@naver.com>
2024-11-04 20:51:22 +0900

    Added some linguistic configurations on neogen.lua

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,7 +1,16 @@
 -- ~/.config/nvim/lua/plugins/neogen.lua
 return {
   "danymat/neogen",
-  config = true,
+  config = function()
+    require("neogen").setup({
+      snippet_engine = "luasnip", -- Optional: Set your preferred snippet engine
+      languages = {
+        c = { template = { annotation_convention = "doxygen" } },
+        go = { template = { annotation_convention = "godoc" } },
+        python = { template = { annotation_convention = "google_docstrings" } },
+      },
+    })
+  end,
   -- Uncomment next line if you want to follow only stable versions
   -- version = "*"
 }
