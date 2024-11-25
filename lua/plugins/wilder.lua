9c954466417e6fe3ac2331566cfbf57ab33542a0
knightchaser <agerio100@naver.com>
2024-11-25 11:11:25 +0900

    Modified wilder.nvim config to look better

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -3,35 +3,41 @@ return {
   "gelguy/wilder.nvim",
   config = function()
     local wilder = require('wilder')
+
     wilder.setup({ modes = { ':', '/', '?' } })
 
-    -- Configure fuzzy matching and pipelines
+    -- Configure pipelines with fuzzy finding
     wilder.set_option('pipeline', {
       wilder.branch(
         wilder.cmdline_pipeline({
-          fuzzy = 1, -- Enable fuzzy matching
+          fuzzy = 1,             -- Enable fuzzy matching
           set_pcre2_pattern = 1, -- Use PCRE2 for better pattern matching
         }),
         wilder.python_search_pipeline({
-          pattern = wilder.python_fuzzy_pattern(),
+          pattern = wilder.python_fuzzy_pattern(), -- Fuzzy search pattern
         })
       ),
     })
 
-    -- Use popupmenu_palette_theme for command palette style
-    wilder.set_option('renderer', wilder.popupmenu_renderer(
-      wilder.popupmenu_palette_theme({
-        border = 'rounded', -- Border style: 'single', 'double', 'rounded', etc.
-        max_height = '75%', -- Maximum height of the popup menu
-        min_height = 0, -- Minimum height (set to 0 for dynamic height)
-        prompt_position = 'top', -- Position of the prompt: 'top' or 'bottom'
-        reverse = 0, -- Display items from top to bottom
-        highlights = {
-          border = 'Normal', -- Highlight group for the border
-        },
-        highlighter = wilder.basic_highlighter(), -- Highlight matches
-      })
-    ))
+    local gradient = {
+      '#f4468f', '#fd4a85', '#ff507a', '#ff566f', '#ff5e63',
+      '#ff6658', '#ff704e', '#ff7a45', '#ff843d', '#ff9036',
+      '#f89b31', '#efa72f', '#e6b32e', '#dcbe30', '#d2c934',
+      '#c8d43a', '#bfde43', '#b6e84e', '#aff05b'
+    }
+
+    for i, fg in ipairs(gradient) do
+      gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', {{a = 1}, {a = 1}, {foreground = fg}})
+    end
+
+    wilder.set_option('renderer', wilder.popupmenu_renderer({
+      highlights = {
+        gradient = gradient, -- Must be set
+        -- The selected_gradient key can be set to apply gradient highlighting for the selected candidate.
+      },
+      highlighter = wilder.highlighter_with_gradient({
+        wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
+      }),
+    }))
   end,
 }
-
