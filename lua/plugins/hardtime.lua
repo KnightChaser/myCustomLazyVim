0f957d145791b89a52f27317e2caad240ac40b4b
knightchaser <agerio100@naver.com>
2024-12-09 11:08:25 +0900

    Install hardtime.lua and automatically enable

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,7 @@
+-- ~/.config/nvim/lua/plugins/hardtime.lua
+return {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", 
+                     "nvim-lua/plenary.nvim" },
+    opts = {}
+}
