0131105051fd5fa0bd74ad07abc06d52b90bc9b1
knightchaser <agerio100@naver.com>
2024-12-16 14:36:47 +0900

    Delete unused makeshift fix of continue using nvim-cmp after 0.14.0 as it changed to blink.cmp

--- init.lua
+++ init.lua
@@ -4,25 +4,17 @@ require("config.lazy")
 require("config.keymaps")
 require("config.options")
 
--- Enable LazyVim extras, including coding.nvim-cmp
-require("lazyvim").setup({
-  extras = {
-    "coding.nvim-cmp", -- Enables nvim-cmp support
-  },
-})
-
 -- Set colorscheme
 vim.cmd([[colorscheme tokyonight-night]])
 
 -- Save shutdown time
 vim.api.nvim_create_autocmd("VimLeavePre", {
-  callback = function()
-    local shutdown_time = os.time()
-    local file = io.open(vim.fn.stdpath("data") .. "/last_shutdown.txt", "w")
-    if file then
-      file:write(shutdown_time)
-      file:close()
-    end
-  end,
+    callback = function()
+        local shutdown_time = os.time()
+        local file = io.open(vim.fn.stdpath("data") .. "/last_shutdown.txt", "w")
+        if file then
+            file:write(shutdown_time)
+            file:close()
+        end
+    end,
 })
-
