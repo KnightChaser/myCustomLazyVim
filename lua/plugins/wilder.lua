ef36e0f3358c29f6a28f6bb7c803d031721c8e57
knightchaser <agerio100@naver.com>
2025-01-07 11:48:06 +0900

    Updated extensions

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -1,43 +1,43 @@
 -- ~/.config/nvim/lua/plugins/wilder.lua
 return {
-  "gelguy/wilder.nvim",
-  config = function()
-    local wilder = require('wilder')
+    "gelguy/wilder.nvim",
+    config = function()
+        local wilder = require('wilder')
 
-    wilder.setup({ modes = { ':', '/', '?' } })
+        wilder.setup({ modes = { ':', '/', '?' } })
 
-    -- Configure pipelines with fuzzy finding
-    wilder.set_option('pipeline', {
-      wilder.branch(
-        wilder.cmdline_pipeline({
-          fuzzy = 1,             -- Enable fuzzy matching
-          set_pcre2_pattern = 1, -- Use PCRE2 for better pattern matching
-        }),
-        wilder.python_search_pipeline({
-          pattern = wilder.python_fuzzy_pattern(), -- Fuzzy search pattern
+        -- Configure pipelines with fuzzy finding
+        wilder.set_option('pipeline', {
+            wilder.branch(
+                wilder.cmdline_pipeline({
+                    fuzzy = 1,   -- Enable fuzzy matching
+                    set_pcre2_pattern = 1, -- Use PCRE2 for better pattern matching
+                }),
+                wilder.python_search_pipeline({
+                    pattern = wilder.python_fuzzy_pattern(), -- Fuzzy search pattern
+                })
+            ),
         })
-      ),
-    })
 
-    local gradient = {
-      '#f4468f', '#fd4a85', '#ff507a', '#ff566f', '#ff5e63',
-      '#ff6658', '#ff704e', '#ff7a45', '#ff843d', '#ff9036',
-      '#f89b31', '#efa72f', '#e6b32e', '#dcbe30', '#d2c934',
-      '#c8d43a', '#bfde43', '#b6e84e', '#aff05b'
-    }
+        local gradient = {
+            '#f4468f', '#fd4a85', '#ff507a', '#ff566f', '#ff5e63',
+            '#ff6658', '#ff704e', '#ff7a45', '#ff843d', '#ff9036',
+            '#f89b31', '#efa72f', '#e6b32e', '#dcbe30', '#d2c934',
+            '#c8d43a', '#bfde43', '#b6e84e', '#aff05b'
+        }
 
-    for i, fg in ipairs(gradient) do
-      gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', {{a = 1}, {a = 1}, {foreground = fg}})
-    end
+        for i, fg in ipairs(gradient) do
+            gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = fg } })
+        end
 
-    wilder.set_option('renderer', wilder.popupmenu_renderer({
-      highlights = {
-        gradient = gradient, -- Must be set
-        -- The selected_gradient key can be set to apply gradient highlighting for the selected candidate.
-      },
-      highlighter = wilder.highlighter_with_gradient({
-        wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
-      }),
-    }))
-  end,
+        wilder.set_option('renderer', wilder.popupmenu_renderer({
+            highlights = {
+                gradient = gradient, -- Must be set
+                -- The selected_gradient key can be set to apply gradient highlighting for the selected candidate.
+            },
+            highlighter = wilder.highlighter_with_gradient({
+                wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
+            }),
+        }))
+    end,
 }
