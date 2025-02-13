3b3bb9bba6b935905059fa7adf7a8603cf2f318a
knightchaser <agerio100@naver.com>
2025-02-13 14:02:27 +0900

    Modify leap.lua configuration to resolve Lazy.nvim keymap conflict

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -3,24 +3,14 @@
 return {
     "ggandor/leap.nvim",
     config = function()
-        -- Set up the default mappings:
-        -- This creates mappings for 's', 'S', and 'gs' in Normal, Visual, and Operator-pending modes.
-        require("leap").create_default_mappings()
+        -- Set up the default mappings (skipped because we are manually setting them below(overriding))
+        -- require("leap").create_default_mappings()
 
-        -- Additional tweaks for a smoother experience:
-
-        -- This helps leap treat, say, different types of quotes or brackets similarly.
-        require("leap").opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
-        -- Here, <enter> moves to the next available target and <backspace> goes back.
-        require("leap.user").set_repeat_keys("<enter>", "<backspace>")
-
-        -- This allows you to jump to a target even in another window (a “spooky action at a distance”).
-        vim.keymap.set({ "n", "x", "o" }, "gs", function()
-            require("leap.remote").action()
-        end, { desc = "Leap remote action" })
-
-        vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-        vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-        vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+        -- Now override any mapping that conflicts
+        -- For instance, if you want to change the mapping for 's':
+        vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)", { desc = "Custom Leap Forward" })
+        -- You can also override other mappings similarly:
+        vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)", { desc = "Custom Leap Backward" })
+        vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)", { desc = "Custom Leap from Window" })
     end,
 }
