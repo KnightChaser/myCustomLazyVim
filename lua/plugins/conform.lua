834344b0d6e9436c5db0ead02df9d9b892aa053c
knightchaser <agerio100@naver.com>
2025-02-13 13:24:30 +0900

    Resolved conform.nvim directly overriding plugin.config, resulting LazyVim formatting break

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,15 +1,14 @@
 -- ~/.config/nvim/lua/plugins/conform.lua
 
 return {
-    'stevearc/conform.nvim',
-    opts = {},
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                -- Conform will run multiple formatters sequentially
-                python = { "black" },
-            },
-        })
-    end,
+  "stevearc/conform.nvim",
+-- "Note: Don't overwrite the default keymaps for nvim-ufo. Instead, add your own keymaps to ~/.config/nvim/lua/plugins/conform.lua."
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "black" },
+    },
+    -- You can add other options here as needed.
+  },
 }
+
