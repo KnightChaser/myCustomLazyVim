86de08cb333c959862548559dfbba306ee590d0a
KnightChaser <agerio100@naver.com>
2025-02-16 12:33:46 +0900

    Enable my neovim blinks the cursor during editing

--- init.lua
+++ init.lua
@@ -9,6 +9,14 @@ require("custom_extensions.random_hex")
 -- Set colorscheme
 vim.cmd([[colorscheme tokyonight-night]])
 
+-- Blink cursor
+vim.o.guicursor = table.concat({
+    "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+    "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+    "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100"
+}, ",")
+
+
 -- Save shutdown time
 vim.api.nvim_create_autocmd("VimLeavePre", {
     callback = function()
