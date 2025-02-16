cc1a11ffb09faa0d20e1864e542114c463f35b31
KnightChaser <agerio100@naver.com>
2025-02-16 12:31:11 +0900

    Deleted hardtime.nvim as I don't need to use it anymore for now

--- lua/plugins/hardtime.lua
+++
@@ -1,7 +0,0 @@
--- ~/.config/nvim/lua/plugins/hardtime.lua
-return {
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", 
-                     "nvim-lua/plenary.nvim" },
-    opts = {}
-}
