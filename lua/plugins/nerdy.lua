31ca0aad7f6f0f3521965dc0f668aa81a6a7df14
knightchaser <agerio100@naver.com>
2025-02-24 12:04:55 +0900

    Installed nerdy.nvim plugin to easily find NerdFont glyphs easily

---
+++ lua/plugins/nerdy.lua
@@ -0,0 +1,10 @@
+-- ~/.config/nvim/plugins/nerdy.lua
+
+return {
+    '2kabhishek/nerdy.nvim',
+    dependencies = {
+        'stevearc/dressing.nvim',
+        'nvim-telescope/telescope.nvim',
+    },
+    cmd = 'Nerdy',
+}
